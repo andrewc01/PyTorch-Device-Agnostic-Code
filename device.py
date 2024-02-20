7d5cddb32121110939573185aeea199d3437d043
Andy <andy@TensorBook.local>
2024-02-21 01:16:28 +0900

    code

---
+++ device.py
@@ -0,0 +1,11 @@
+import torch
+
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda') # nvidia gpu
+    elif torch.backends.mps.is_available():
+        return torch.device('mps') # apple silicon gpu
+    else:
+        return torch.device('cpu')
+    
+device = get_default_device()
