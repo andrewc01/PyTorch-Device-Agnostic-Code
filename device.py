d8ac3f77bd0f2b57b4d9bc27a1a0ef9f00900e76
Andy <andy@TensorBook.local>
2024-02-21 01:31:50 +0900

    deleted

--- device.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def get_default_device():
-    if torch.cuda.is_available():
-        return torch.device('cuda') # nvidia gpu
-    elif torch.backends.mps.is_available():
-        return torch.device('mps') # apple silicon gpu
-    else:
-        return torch.device('cpu') # default
